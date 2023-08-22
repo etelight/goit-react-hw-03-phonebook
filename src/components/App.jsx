789958a856afc8302d84f97393d8c6f0a6b77620
etelight <etelight@gmail.com>
2023-08-22 14:14:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -50,7 +50,7 @@ export class App extends Component {
       this.setState({ contacts });
     }
   }
-  S;
+
   componentDidUpdate(prevProps, prevState) {
     if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
